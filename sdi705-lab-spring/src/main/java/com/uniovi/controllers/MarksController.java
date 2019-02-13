6e7f0c912dddb9a5ae8f75fe272e2e70efcdda1d
Daniel <daniferna@outlook.com>
2019-02-13 21:08:09 +0100

    SDI - 1.9 Beans - Servicios, inyección de dependencias

--- sdi705-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi705-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,5 +1,6 @@
 package com.uniovi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,25 +8,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.services.MarkServices;
 
 @RestController
 public class MarksController {
+	
+	@Autowired //Inyectamos el servicio
+	private MarkServices markService;
 
 	@RequestMapping("/mark/list")
 	public String getList() {
-		return "Getting List";
+		return markService.getMarks().toString();
 	}
 
 	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
 	public String setMark(@ModelAttribute Mark mark) {
-		return "Added: " + mark.getDescription()
-		+ " with score: " + mark.getScore()
-		+ " id: " + mark.getId();
+		markService.addMark(mark);
+		return "OK";
 	}
 
 	@RequestMapping("/mark/details/{id}")
 	public String getDetail(@PathVariable Long id) {
-		return "Getting Details " + id;
+		return markService.getMark(id).toString();
+	}
+	
+	@RequestMapping("/mark/delete/{id}")
+	public String deleteMark(@PathVariable Long id) {
+		markService.deleteMark(id);
+		return "OK";
 	}
 
 }

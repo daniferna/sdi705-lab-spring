4733b49863008cc9d6cb61d9199c9da64c513bc1
Daniel <daniferna@outlook.com>
2019-02-21 00:18:38 +0100

    SDI-SpringBoot- 6 - Validación de datos en el servidor

--- sdi705-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi705-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -5,10 +5,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
@@ -19,13 +22,21 @@ public class UsersController {
 	@Autowired
 	private SecurityService securityService;
 
-	@RequestMapping(value = "/signup", method = RequestMethod.GET) // PREGUNTAR DIFERENCIAS CON POST
-	public String signup() {
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
 		return "signup";
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@ModelAttribute("user") User user, Model model) {
+	public String signup(@Validated User user, BindingResult result) {
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors())
+			return "signup";
+
 		usersService.addUser(user);
 		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
 		return "redirect:home";
